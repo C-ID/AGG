3f014d5137b80585effc4e1b16237e299f8a9c60
C-ID <invincibleyao@hotmail.com>
2020-10-12 15:57:19 +0800

    finish coding, start debuging

--- contracts/ExchangeBase.sol
+++ contracts/ExchangeBase.sol
@@ -1,14 +1,16 @@
 
 pragma solidity ^0.5.0;
-import "./interface/IUniswapV2Factory.sol"
+import "./interface/IUniswapV2Factory.sol";
 
 contract ExchangeBase is IERC20 {
 
     IUniswapV2Factory constant internal uniswapV2 = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f); 
-    
-    struct Array{
-        
+    mapping (string => address) exchangeList;
+
+    constructor(exchangeList _exchangelist) public {
+        _exchangelist["uniswapv2"] = uniswapV2
     }
+    
     function _getMulitExpectedReturn(
         IERC20[] fromToken,
         IERC20[] destToken,
@@ -26,19 +28,27 @@ contract ExchangeBase is IERC20 {
     function parsingTokenList(
         IERC20[] fromToken,
         IERC20[] destToken
-    ) internal view returns (IERC20[][] pair)
+    ) internal view returns (IERC20[][] pair, bool reverse)
     {
-        if(from.length > 1 && destToken.length==1)
+        reverse = false;
+        if(fromToken.length > 1 && destToken.length==1)
         {
-            
+            for(uint i=0; i<fromToken.length; i++)
+            {
+                pair[fromToken[i]] = destToken[0];
+            }
         }
-        else if(from.length==1 && destToken.length>1)
+        else if(fromToken.length==1 && destToken.length>1)
         {
-
+            for(uint i=0; i<destToken.length; i++)
+            {
+                pair[destToken[i]] = fromToken[0];
+            }
+            reverse = true;
         }
         else if(fromToken.length == 1 && destToken.length==1)
         {
-
+            pair[fromToken[0]] = fromToken[0]
         }
     }
 }
