dbe1845ee4409a18e995e77affcbc7998e78f63f
C-ID <invincibleyao@hotmail.com>
2020-10-15 19:26:26 +0800

    change WETH address

--- contracts/uniswapv2/uniswapv2Exchange.sol
+++ contracts/uniswapv2/uniswapv2Exchange.sol
@@ -13,7 +13,8 @@ contract Uniswapv2Exchange{
     using SafeMath for uint256;
     
     IUniswapV2Factory constant internal uniswapV2 = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
-    
+    //WETH address
+    IWETH constant internal weth = IWETH(0xc778417E063141139Fce010982780140Aa0cD5Ab);
     // function create(
     //     IERC20 fromToken,
     //     IERC20 destToken,){
@@ -24,13 +25,16 @@ contract Uniswapv2Exchange{
         IERC20 destToken,
         uint256 amounts
     ) internal view returns(uint256 rets) {
-        // IERC20 fromTokenReal = fromToken.isETH() ? weth : fromToken;
-        // IERC20 destTokenReal = destToken.isETH() ? weth : destToken;
-        IUniswapV2Exchange exchange = uniswapV2.getPair(fromToken, destToken);
-        // emit log(exchange);
+        IERC20 fromTokenReal = fromToken.isETH() ? weth : fromToken;
+        IERC20 destTokenReal = destToken.isETH() ? weth : destToken;
+        // uint len = uniswapV2.allPairsLength();
+        // uint first = 0;
+        // address pair = uniswapV2.allPairs(first);
+        IUniswapV2Exchange exchange = uniswapV2.getPair(fromTokenReal, destTokenReal);
+
         if (exchange != IUniswapV2Exchange(0)) {
-            uint256 fromTokenBalance = fromToken.universalBalanceOf(address(exchange));
-            uint256 destTokenBalance = destToken.universalBalanceOf(address(exchange));
+            uint256 fromTokenBalance = fromTokenReal.universalBalanceOf(address(exchange));
+            uint256 destTokenBalance = destTokenReal.universalBalanceOf(address(exchange));
             rets = _calculateUniswapFormula(fromTokenBalance, destTokenBalance, amounts);
             return (rets);
         }
@@ -148,16 +152,16 @@ contract Uniswapv2Exchange{
         IERC20 destToken,
         uint256 amount
     ) internal returns(uint256 returnAmount) {
-        // if (fromToken.isETH()) {
-        //     weth.deposit.value(amount)();
-        // }
+        if (fromToken.isETH()) {
+            weth.deposit.value(amount)();
+        }
         // IUniswapV2Exchange exchange = IUniswapV2Exchange(0xa6f3ef841d371a82ca757FaD08efc0DeE2F1f5e2);
-        // IERC20 fromTokenReal = fromToken.isETH() ? weth : fromToken;
-        // IERC20 toTokenReal = destToken.isETH() ? weth : destToken;
-        IUniswapV2Exchange exchange = uniswapV2.getPair(fromToken, destToken);
+        IERC20 fromTokenReal = fromToken.isETH() ? weth : fromToken;
+        IERC20 toTokenReal = destToken.isETH() ? weth : destToken;
+        IUniswapV2Exchange exchange = uniswapV2.getPair(fromTokenReal, toTokenReal);
         bool needSync;
         bool needSkim;
-        (returnAmount, needSync, needSkim) = exchange.getReturn(fromToken, destToken, amount);
+        (returnAmount, needSync, needSkim) = exchange.getReturn(fromTokenReal, toTokenReal, amount);
         if (needSync) {
             exchange.sync();
         }
@@ -165,16 +169,16 @@ contract Uniswapv2Exchange{
             exchange.skim(0x68a17B587CAF4f9329f0e372e3A78D23A46De6b5);
         }
 
-        fromToken.universalTransfer(address(exchange), amount);
-        if (uint256(address(fromToken)) < uint256(address(destToken))) {
+        fromTokenReal.universalTransfer(address(exchange), amount);
+        if (uint256(address(fromTokenReal)) < uint256(address(toTokenReal))) {
             exchange.swap(0, returnAmount, address(this), "");
         } else {
             exchange.swap(returnAmount, 0, address(this), "");
         }
 
-        // if (destToken.isETH()) {
-        //     weth.withdraw(weth.balanceOf(address(this)));
-        // }
+        if (destToken.isETH()) {
+            weth.withdraw(weth.balanceOf(address(this)));
+        }
     }
 
 }
--- interfaces/uniswapv2/IUniswapV2Factory.sol
+++ interfaces/uniswapv2/IUniswapV2Factory.sol
@@ -7,4 +7,6 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contr
 interface IUniswapV2Factory {
     function getPair(IERC20 tokenA, IERC20 tokenB) external view returns (IUniswapV2Exchange pair);
     function createPair(IERC20 tokenA, IERC20 tokenB) external returns (IUniswapV2Exchange pair);
+    function allPairs(uint) external view returns (address pair);
+    function allPairsLength() external view returns (uint);
 }
